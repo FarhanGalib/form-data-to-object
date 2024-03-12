f24d33770ba1f1c395492d82135f4b34d673c4cb
Farhan Galib <galib.exabyting@bkash.com>
2024-03-12 12:08:26 +0600

    Convert form data to object.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [userData, setUserData] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formDataObject = Object.fromEntries(formData.entries());
+    setUserData(formDataObject);
+    console.log(formDataObject);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          position: 'absolute',
+          border: '1px solid black',
+          display: 'flex',
+          flexDirection: 'column',
+          width: '40%',
+          gap: '.5rem',
+          padding: '1rem',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <input name='email' type='text' placeholder='email' />
+        <input name='name' type='text' placeholder='name' />
+        <input type='submit' />
+      </form>
+      <div>{userData?.email}</div>
+      <div>{userData?.name}</div>
     </div>
   );
 }
